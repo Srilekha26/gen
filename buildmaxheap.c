144389c95925675b2ae8ab7737448568d194f226
Srilekha.Talluri <30970454+Srilekha26@users.noreply.github.com>
2018-06-07 11:54:07 +0530

    Create buildmaxheap.c

---
+++ buildmaxheap.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+void swap(int *a,int *b){
+    int temp;
+    temp=*a;
+    *a=*b;
+    *b=temp;
+}
+void maxheap(int*a,int i,int n){
+    if(2*i>n) return ;
+    else{
+        maxheap(a,2*i,n);
+        maxheap(a,2*i+1,n);
+    }if(a[i]<a[2*i]){
+        if(a[2*i]<a[2*i+1]) swap(&a[i],&a[2*i+1]);
+        else swap(&a[i],&a[2*i]);
+    }else if(a[i]<a[2*i+1]) swap(&a[i],&a[2*i+1]);
+    else return ;
+}
+int main(){
+   int n;
+   scanf("%d",&n);
+   int i,a[n+1];
+   for(i=1;i<n+1;i++){
+       scanf("%d",&a[i]);
+   }
+   maxheap(a,1,7);
+   for(i=1;i<n+1;i++)
+   printf(" %d",a[i]);
+   
+}
