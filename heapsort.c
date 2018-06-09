9e1b3d6b1bb5f7d3cf67e7a9b39bc477e1993c16
Srilekha.Talluri <30970454+Srilekha26@users.noreply.github.com>
2018-06-09 12:29:41 +0530

    Update heapsort.c

--- heapsort.c
+++ heapsort.c
@@ -20,11 +20,29 @@ void maxheap(int*a,int i,int n){
     }else if(a[i]<a[2*i+1]) swap(&a[i],&a[2*i+1]);
     else return ;
 }
+void maxheapify(int*a,int i,int n){
+    int largest;
+    if(2*i>n) return;
+    if(2*i==n){
+         if(a[i]<a[2*i]) swap(&a[i],&a[2*i]);
+        return ;
+    }
+    if(a[i]<a[2*i]){
+        swap(&a[i],&a[2*i]);
+        largest=2*i;
+    }if(a[i]<a[2*i+1]) {
+        swap(&a[i],&a[2*i+1]);
+        largest=2*i+1;
+    } if(largest!=i){
+        maxheapify(a,largest,n);
+    } return ;
+}    
 void heapsort(int*a,int n){
     int i=0;
-     while(n-i>2){
-   maxheap(a,1,n-i);
-   printf("%d ",a[1]);
+    maxheap(a,1,n-i);
+     while(n>i){
+     maxheapify(a,1,n-i);
+ printf(" %d ",a[1]);
    a[1]=a[n-i];
    a[n-i]=-1;
    i++;
